f5b7e603161bc64a2915fc1c22300d9c29808578
Eric <eric.lorenzo@gracia.lasalle.cat>
2025-12-18 18:55:40 +0100

    Setting environment configuration

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
+# Amunt
+
 def on_up_pressed():
     animation.run_image_animation(nena,
         assets.animation("""
             nena-animation-up
             """),
-        500,
-        False)
+        100,
+        True)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+
+# ANIMACIONS NENA
+# Esquerra
 
 def on_left_pressed():
     animation.run_image_animation(nena,
         assets.animation("""
             nena-animation-left
             """),
-        500,
-        False)
+        100,
+        True)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
+# Dreta
 
 def on_right_pressed():
     animation.run_image_animation(nena,
         assets.animation("""
             nena-animation-right
             """),
-        500,
-        False)
+        100,
+        True)
 controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+
+# Abaix
 
 def on_down_pressed():
     animation.run_image_animation(nena,
         assets.animation("""
             nena-animation-down
             """),
-        500,
-        False)
+        100,
+        True)
 controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
 
 nena: Sprite = None
+tiles.set_current_tilemap(tilemap("""
+    map
+    """))
 nena = sprites.create(assets.image("""
     nena-front
     """), SpriteKind.player)
-controller.move_sprite(nena)
+nena.set_position(130, 250)
+scene.camera_follow_sprite(nena)
+controller.move_sprite(nena, 100, 100)
--- main.ts
+++ main.ts
@@ -1,35 +1,50 @@
+namespace SpriteKind {
+    export const edifici = SpriteKind.create()
+}
+// Amunt
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     nena,
     assets.animation`nena-animation-up`,
-    500,
-    false
+    100,
+    true
     )
 })
+// ANIMACIONS NENA
+// Esquerra
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     nena,
     assets.animation`nena-animation-left`,
-    500,
-    false
+    100,
+    true
     )
 })
+// Dreta
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     nena,
     assets.animation`nena-animation-right`,
-    500,
-    false
+    100,
+    true
     )
 })
+// Abaix
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     nena,
     assets.animation`nena-animation-down`,
-    500,
-    false
+    100,
+    true
     )
 })
 let nena: Sprite = null
+tiles.setCurrentTilemap(tilemap`map`)
+let casa_intercanvi = sprites.create(assets.image`casa_intercanvi`, SpriteKind.edifici)
+casa_intercanvi.setPosition(129, 130)
+let venedor = sprites.create(assets.image`venedor-front`, SpriteKind.Player)
+venedor.setPosition(129, 140)
 nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
-controller.moveSprite(nena)
+nena.setPosition(129, 250)
+scene.cameraFollowSprite(nena)
+controller.moveSprite(nena, 100, 100)
